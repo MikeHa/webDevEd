8c0902aff851e0560180799d1fc9846139569ab3
MikeHa <mikemhn@gmail.com>
2017-08-16 02:05:49 -0700

    added new functionality to JS_ToDoList

--- webDevBootcamp/JS_ToDoList/script.js
+++ webDevBootcamp/JS_ToDoList/script.js
@@ -4,9 +4,18 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
 	if(input === "list") {
-		console.log(todos);
+		console.log("**********")
+		todos.forEach(function(todo, i){
+		console.log(i + ": " + todo);
+	});
+		console.log("**********")
 	} else if(input === "new") {
 		todos.push(prompt("What would you like to add?"));
+	} else if (input === "delete") {
+		// ash for index for todo to be deleted
+			var index = prompt('Enter index of todo to delete');
+		// delete that todo
+			todos.splice(index,1);
 	}
 	input = prompt("What would you like to do?");
 }
