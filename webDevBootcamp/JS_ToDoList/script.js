a0e153b1dcacfc73f240b6d5b4d5024f6241c888
MikeHa <mikemhn@gmail.com>
2017-08-17 00:42:54 -0700

    finished arrayProblemSet

--- webDevBootcamp/JS_ToDoList/script.js
+++ webDevBootcamp/JS_ToDoList/script.js
@@ -4,19 +4,33 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit"){
 	if(input === "list") {
-		console.log("**********")
-		todos.forEach(function(todo, i){
-		console.log(i + ": " + todo);
-	});
-		console.log("**********")
+		listTodos();
 	} else if(input === "new") {
-		todos.push(prompt("What would you like to add?"));
+		addTodo();
 	} else if (input === "delete") {
-		// ash for index for todo to be deleted
-			var index = prompt('Enter index of todo to delete');
-		// delete that todo
-			todos.splice(index,1);
+		deleteTodo();
 	}
 	input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT");
+
+function listTodos() {
+	console.log("**********")
+	todos.forEach(function(todo, i){
+	console.log(i + ": " + todo);
+});
+	console.log("**********")
+}
+
+function addTodo() {
+	todos.push(prompt("What would you like to add?"));
+	console.log("Added to do")
+}
+
+function deleteTodo() {
+	// ash for index for todo to be deleted
+		var index = prompt('Enter index of todo to delete');
+	// delete that todo
+		todos.splice(index,1);
+		console.log("Deleted todo");
+}
