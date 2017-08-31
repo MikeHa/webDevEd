722a7f073b7e4d24ff961568a99ec1b42ae363e0
MikeHa <mikemhn@gmail.com>
2017-08-31 01:38:33 -0700

    init Project_ToDoList

---
+++ webDev/Project_ToDoList/assets/js/script.js
@@ -0,0 +1,23 @@
+// Check off specific Todos by clicking
+// $("li").click(function(){
+//   //if li is gray
+//   if($(this).css("color") === "rgb(128, 128, 128)") {
+//     //turn it black
+//     $(this).css({
+//       color: "black",
+//       textDecoration: "none"
+//     });
+//   }
+//   //else
+//   else{
+//     //turn it gray
+//     $(this).css({
+//       color: "gray",
+//       textDecoration: "line-through"
+//     });
+//     }
+// });
+
+$("li").click(function(){
+  $(this).toggleClass("completed");
+});
