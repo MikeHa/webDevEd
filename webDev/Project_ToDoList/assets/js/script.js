ebdb915374a8289ec74c95c01239cd69e7ace98d
MikeHa <mikemhn@gmail.com>
2017-09-03 22:05:34 -0700

    setup files for Project_ToDoList

--- webDev/Project_ToDoList/assets/js/script.js
+++ webDev/Project_ToDoList/assets/js/script.js
@@ -18,6 +18,24 @@
 //     }
 // });
 
-$("li").click(function(){
+$("ul").on("click", "li", function(){
   $(this).toggleClass("completed");
+});
+
+// Click on X to delete Todos
+$("ul").on("click", "span", function(event){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if(event.which === 13){
+    // grabs new todo text from inut
+    var todoText = $(this).val();
+    $(this).val("");
+    // create a new li and add to ul
+    $("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+  }
 });
