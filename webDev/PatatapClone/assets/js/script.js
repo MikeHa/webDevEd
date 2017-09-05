6c60a9034a46406329f6b5052002a06eb6f44368
MikeHa <mikemhn@gmail.com>
2017-09-04 20:43:24 -0700

    Finished Patatap Clone

--- webDev/PatatapClone/assets/js/script.js
+++ webDev/PatatapClone/assets/js/script.js
@@ -1,20 +1,20 @@
-var circles = [];
+// var circles = [];
 
-function onKeyDown(event) {
-  var maxPoint = new Point(view.size.width, view.size.height);
-  var randomPoint = Point.random();
-  var point = maxPoint * randomPoint;
-  circles.push(new Path.Circle(point, 10).fillColor = "orange");
-}
+// function onKeyDown(event) {
+//   var maxPoint = new Point(view.size.width, view.size.height);
+//   var randomPoint = Point.random();
+//   var point = maxPoint * randomPoint;
+//   circles.push(new Path.Circle(point, 10).fillColor = "orange");
+// }
 
-var animatedCircle = new Path.Circle(new Point(300, 300), 100);
-animatedCircle.fillColor = "red";
+// var animatedCircle = new Path.Circle(new Point(300, 300), 100);
+// animatedCircle.fillColor = "red";
 
-function onFrame(event) {
-  for (var i = 0; i < circles.length; i++) {
-    circles[i].fillColor.hue += 1;
-  }
+// function onFrame(event) {
+//   for (var i = 0; i < circles.length; i++) {
+//     circles[i].fillColor.hue += 1;
+//   }
 
-  animatedCircle.fillColor.hue += 1;
-  animatedCircle.scale(0.9);
-}
+//   animatedCircle.fillColor.hue += 1;
+//   // animatedCircle.scale(0.9);
+// }
