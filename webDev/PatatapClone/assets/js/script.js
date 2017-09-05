76bb46cf76dcefea762c31f407d0ba5644e85139
MikeHa <mikemhn@gmail.com>
2017-09-04 17:54:29 -0700

    Added multiple circles

--- webDev/PatatapClone/assets/js/script.js
+++ webDev/PatatapClone/assets/js/script.js
@@ -1,13 +1,20 @@
-  // // Create a Paper.js Path to draw a line into it:
-  // var path = new Path();
-  // // Give the stroke a color
-  // path.strokeColor = 'black';
-  // var start = new Point(0, 100);
-  // // Move to start and draw a line from there
-  // path.moveTo(start);
-  // // Note the plus operator on Point objects.
-  // // PaperScript does that for us, and much more!
-  // path.lineTo(start + [100, -50]);
-  
-  // var myCircle = new Path.Circle(new Point(100, 70), 50);
-  // myCircle.fillColor = 'black';
+var circles = [];
+
+function onKeyDown(event) {
+  var maxPoint = new Point(view.size.width, view.size.height);
+  var randomPoint = Point.random();
+  var point = maxPoint * randomPoint;
+  circles.push(new Path.Circle(point, 10).fillColor = "orange");
+}
+
+var animatedCircle = new Path.Circle(new Point(300, 300), 100);
+animatedCircle.fillColor = "red";
+
+function onFrame(event) {
+  for (var i = 0; i < circles.length; i++) {
+    circles[i].fillColor.hue += 1;
+  }
+
+  animatedCircle.fillColor.hue += 1;
+  animatedCircle.scale(0.9);
+}
